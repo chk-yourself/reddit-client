5e88e8497c0a3456a701c2126fc1c35a768ceceb
Christina Kim <christinahyunakim@gmail.com>
2020-04-19 15:37:41 -0700

    Updates

---
+++ precache-manifest.d71afa2a1e077eab409c7cb8b78ce318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24fbbc44522410bb98cefbd760f57ba7",
+    "url": "/reddit-client/index.html"
+  },
+  {
+    "revision": "cfc8549d4d89de85eea7",
+    "url": "/reddit-client/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b6db0f7f9dd3681c559e",
+    "url": "/reddit-client/static/js/2.0a2ba44a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reddit-client/static/js/2.0a2ba44a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc8549d4d89de85eea7",
+    "url": "/reddit-client/static/js/main.8df7a175.chunk.js"
+  },
+  {
+    "revision": "4e3241e97d9f8de5ceee",
+    "url": "/reddit-client/static/js/runtime-main.b66d054d.js"
+  }
+]);
